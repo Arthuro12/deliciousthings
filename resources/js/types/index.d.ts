3b98c2efab702bc1056385e6f3870b758f7f8daa
Arthur Foadjo <arthurfoadjo@yahoo.com>
2025-11-16 15:29:18 +0100

    refactor: Refactor types

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -1,6 +1,4 @@
-export interface Auth {
-    user: User;
-}
+import type { Auth } from "./users"; 
 
 export type AppPageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {
     name: string;
@@ -8,48 +6,3 @@ export type AppPageProps<T extends Record<string, unknown> = Record<string, unkn
     auth: Auth;
 };
 
-export type Address = {
-    street: string;
-    houseNumber: string;
-    postalCode?: string | null;
-    city: string;
-    country: string;
-    addressLine2: string | null;
-}
-
-export type Speciality = {
-    key: string;
-    name: string;
-    label: string;
-}
-
-export type Media = {
-    category: string | null;
-    caption: string | null;
-    path: string;
-};
-
-export interface User {
-    id: number;
-    name: string;
-    email: string;
-    media?: string;
-}
-
-export type ArtisanProfile = {
-    username: string;
-    company_name: string;
-    e164phone?: string;
-    email: string;
-    biography: string;
-    medias: Media[];
-    specialities: Speciality[];
-    address?: Address;
-    instagram_url?: string;
-    website_url?: string;
-    average_rate: string;
-    offers_delivery: boolean;
-}
-
-export type ClassType = string | Record<string, boolean>;
-export type VueClass = ClassType | ClassType[] | VueClass[];
---
+++ resources/js/types/ui.ts
@@ -0,0 +1,2 @@
+export type ClassType = string | Record<string, boolean>;
+export type VueClass = ClassType | ClassType[] | VueClass[];
