a87002be7320088d458db7b5c32bdc68d71abcea
Arthur Foadjo <arthurfoadjo@yahoo.com>
2025-11-19 19:10:30 +0100

    refactor: Update method comment

--- resources/js/api.ts
+++ resources/js/api.ts
@@ -1,5 +1,8 @@
 import type { Response } from "@/types/api";
 
+/**
+ * Sends a GET request to retrieve data from server.
+ */
 export async function get<T>(url: string): Promise<Response<T>> {
     let error = null;
     let data = null;
