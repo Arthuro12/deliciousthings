a10f2c23f94f4a57917085f38645d9953c15ad28
Arthur Foadjo <arthurfoadjo@yahoo.com>
2025-11-22 20:21:07 +0100

    refactor: Remove dead code

--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,3 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\SpecialityController;
 
 Route::get('/specialities', [SpecialityController::class, 'index'])->middleware('auth:sanctum');
-Route::post('/specialities', [SpecialityController::class, 'store'])->middleware('auth:sanctum');
