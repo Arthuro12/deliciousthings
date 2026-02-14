7a1142a574bfa783882b87e480ad73d730f1c38e
Arthur Foadjo <arthurfoadjo@yahoo.com>
2026-02-14 20:39:48 +0100

    feat(artisan-search): Add `AddressSearchInput` component

--- resources/js/components/search/ArtisansSearch.vue
+++ resources/js/components/search/ArtisansSearch.vue
@@ -13,15 +13,24 @@
                     <SlidersHorizontalIcon />
                 </button>
             </template>
-            <div></div>
+            <template #default>
+                <div>
+                    <div class="address-autocomplete-form">
+						<AddressSearchInput />
+                    </div>
+                </div>
+            </template>
         </RekaDialog>
     </div>
 </template>
 
 <script setup lang="ts">
-import {  SlidersHorizontalIcon } from "lucide-vue-next";
+import { SlidersHorizontalIcon } from "lucide-vue-next";
 
 import RekaDialog from "@/third-party/reka-ui/RekaDialog.vue";
+import RekaSelect from "@/third-party/reka-ui/RekaSelect.vue";
+import AddressSearchInput from "./AddressSearchInput.vue";
+
 </script> 
 
 <style scoped lang="scss">
@@ -29,5 +38,9 @@ import RekaDialog from "@/third-party/reka-ui/RekaDialog.vue";
     background-color: var(--color-neutral-0);
     color: var(--color-primary-50);
     border-radius: 24px;
+}
+
+.address-autocomplete-form {
+	margin: 24px 0;
 }
 </style>
