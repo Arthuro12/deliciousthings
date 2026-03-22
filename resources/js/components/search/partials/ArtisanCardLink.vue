d1b4d487406fb1d3d0c098598a205087c94b85ce
Arthur Foadjo <arthurfoadjo@yahoo.com>
2026-03-22 22:40:14 +0100

    Add changes to test workflow

--- resources/js/components/search/partials/ArtisanCardLink.vue
+++ resources/js/components/search/partials/ArtisanCardLink.vue
@@ -12,6 +12,7 @@
             </div>
             <div class="artisan-detail">
                 <span>{{ priceDetails }}</span>
+                <span v-if="artisan.offers_delivery">Lieferdienst angeboten</span>
             </div>
         </div>
     </a>
