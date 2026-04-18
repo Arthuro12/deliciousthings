e28e6a67429b7ca987f38f3edfe63aec050709f6
Arthur Foadjo <arthurfoadjo@yahoo.com>
2026-04-18 13:56:27 +0200

    Fix password input component appearance on mobile

--- resources/js/components/presentation/PasswordInput.vue
+++ resources/js/components/presentation/PasswordInput.vue
@@ -59,7 +59,7 @@ const showRevealIcon = computed(() => !!(modelValue.value && modelValue.value.le
 @use "../../../css/abstracts/breakpoints" as breakpoints;
 
 input[type="password"] {
-    ::-ms-reveal {
+    &::-ms-reveal {
         opacity: 0;
     }
     
@@ -86,6 +86,7 @@ input[type="password"] {
     .password-input {
         border: none;
         outline: none;
+        padding: 0;
         width: 85%;
 
         @include breakpoints.respond-to("medium") {
